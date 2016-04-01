af9dddbe40c3c78e32d23dea7b4d7a7bbb98f12d
Wagner F O Jr <wagnerju@me.com>
2016-04-01 17:31:22 -0300

    initial files

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnConectar = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBoxReceber = new System.Windows.Forms.TextBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnConectar
+            // 
+            this.btnConectar.Location = new System.Drawing.Point(10, 10);
+            this.btnConectar.Name = "btnConectar";
+            this.btnConectar.Size = new System.Drawing.Size(75, 23);
+            this.btnConectar.TabIndex = 0;
+            this.btnConectar.Text = "Conectar";
+            this.btnConectar.UseVisualStyleBackColor = true;
+            this.btnConectar.Click += new System.EventHandler(this.btnConectar_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(109, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(124, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // textBoxReceber
+            // 
+            this.textBoxReceber.Location = new System.Drawing.Point(10, 56);
+            this.textBoxReceber.Multiline = true;
+            this.textBoxReceber.Name = "textBoxReceber";
+            this.textBoxReceber.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxReceber.Size = new System.Drawing.Size(302, 192);
+            this.textBoxReceber.TabIndex = 4;
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(254, 10);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(58, 23);
+            this.btnRefresh.TabIndex = 5;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(324, 260);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.textBoxReceber);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btnConectar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConectar;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBoxReceber;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button btnRefresh;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.IO;
+using System.Media;
+using SerialPortListener.Serial;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+
+        private SoundPlayer soundPlayer;
+        private string soundsPath;
+
+        private SerialPortManager _spManager;
+        private SerialSettings mySerialSettings;
+
+        public Form1()
+        {
+            InitializeComponent();
+            UserInitialization();
+        }
+
+        #region Methods
+
+        private void UserInitialization()
+        {
+            _spManager = new SerialPortManager();
+            mySerialSettings = _spManager.CurrentSerialSettings;
+
+            _spManager.NewSerialDataRecieved += new EventHandler<SerialDataEventArgs>(_spManager_NewSerialDataRecieved);
+            this.FormClosing += new FormClosingEventHandler(MainForm_FormClosing);
+
+            refreshCOMList();
+
+            soundPlayer = new SoundPlayer();
+            soundsPath = Application.StartupPath + "\\sounds\\";
+            Console.WriteLine("Exec path: \n" + soundsPath);
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            refreshCOMList();
+
+            return;
+        }
+
+        private void refreshCOMList()
+        {
+            comboBox1.Items.Clear();
+
+            foreach (string s in SerialPort.GetPortNames())
+            {
+                comboBox1.Items.Add(s);
+            }
+
+            if (comboBox1.Items.Count > 0)
+                comboBox1.SelectedIndex = 0;
+
+            return;
+        }
+
+        private void btnConectar_Click(object sender, EventArgs e)
+        {
+
+            #region Button Conectar
+        
+            if(comboBox1.SelectedIndex == -1)
+            {
+                textBoxReceber.AppendText("Selecione uma porta!");
+                return;
+            }
+            
+                mySerialSettings.PortName = comboBox1.Items[comboBox1.SelectedIndex].ToString();
+
+            if (!_spManager.isOpen)
+            {
+                try
+                {
+                    textBoxReceber.AppendText("Conectando a porta " + mySerialSettings.PortName + " ...");
+                    _spManager.StartListening();
+
+                }
+                catch (IOException ex)
+                {
+                    textBoxReceber.AppendText("Não foi possível conectar: ");
+                    Console.WriteLine(ex.ToString());
+                    return;
+                }
+
+                if (_spManager.isOpen)
+                {
+                    btnConectar.Text = "Desconectar";
+                    textBoxReceber.AppendText("Conectado.\n Dados recebidos: ");
+                    comboBox1.Enabled = false;
+                }
+            }
+            else
+            {
+                try
+                {
+                    _spManager.StopListening();
+                    comboBox1.Enabled = true;
+                    btnConectar.Text = "Conectar";
+                    textBoxReceber.AppendText("Desconectado.");
+                }
+                catch
+                {
+                    return;
+                }
+            }
+            #endregion
+
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            _spManager.Dispose();
+        }
+
+        private void trataDadoRecebido(string dado)
+        {
+            if (dado.Equals("1"))
+            {
+                soundPlayer.SoundLocation = soundsPath + "o.wav";
+                soundPlayer.Play();
+            }
+        }
+
+        #endregion
+
+        #region Events Handlers
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            _spManager.Dispose();
+        }
+
+        void _spManager_NewSerialDataRecieved(object sender, SerialDataEventArgs e)
+        {
+            if (this.InvokeRequired)
+            {
+                // Using this.Invoke causes deadlock when closing serial port, and BeginInvoke is good practice anyway.
+                this.BeginInvoke(new EventHandler<SerialDataEventArgs>(_spManager_NewSerialDataRecieved), new object[] { sender, e });
+                return;
+            }
+
+            int maxTextLength = 1000; // maximum text length in text box
+            if (textBoxReceber.TextLength > maxTextLength)
+                textBoxReceber.Text = textBoxReceber.Text.Remove(0, textBoxReceber.TextLength - maxTextLength);
+
+            // This application is connected to a GPS sending ASCCI characters, so data is converted to text
+            string str = Encoding.ASCII.GetString(e.Data);
+
+            trataDadoRecebido(str);
+
+            textBoxReceber.AppendText(str + " ");
+            textBoxReceber.ScrollToCaret();
+     
+        }
+        #endregion
+
+    }
+}
